e98a0a22397a6cc0f955a7eecadcf474f2a491fa
lewismunene020 <munene.lewis@students.jkuat.ac.ke>
2022-12-22 13:05:34 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import {SERVER_URL} from './Utils'
+import {useState , useEffect} from 'react'
+
+function App() {
+  const [languages  , setLanguages]  =  useState(undefined)
+ 
+ useEffect(()=>{
+  fetchData(SERVER_URL+"/languages" ,setLanguages)
+/*  let url = SERVER_URL+"/languages";
+
+  fetch(url)
+   .then((response)=>response.json())
+   .then(json=>{
+	setLanguages(json.data)
+        console.log(json.data)
+     
+   }).catch((error)=>{
+     alert(error);
+  });*/
+
+      
+  },[]);
+  return (
+    
+      <header className="App-header">
+         <h3> Programming Languages Learnt</h3>
+        {languages ? (
+	  <ul>
+	   {languages.map((value ,index)=>{
+	      return  <li>{value.name}</li>;
+          })}
+	    
+	  </ul>
+	  ): (
+		<p>No languages found</p>
+	  )}
+     </header>
+    
+  );
+}
+
+function fetchData(url ,callBack){	
+fetch(url)
+   .then((response)=>response.json())
+   .then((json)=>{
+	alert(json)
+     callBack(json);   
+   });
+
+
+
+}
+
+export default App;
---
+++ client/src/Utils.js
@@ -0,0 +1,13 @@
+ const  SERVER_URL = 'http://127.0.0.1:5000/api';
+
+
+
+
+
+
+
+export {SERVER_URL}
+
+
+
+
